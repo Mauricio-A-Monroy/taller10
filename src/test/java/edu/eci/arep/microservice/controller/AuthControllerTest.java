6dcd8c2adaba8a9e73cfdfbe2f921029be1f1c9c
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2025-03-31 21:46:28 -0500

    commit

--- src/test/java/edu/eci/arep/microservice/controller/AuthControllerTest.java
+++ src/test/java/edu/eci/arep/microservice/controller/AuthControllerTest.java
@@ -9,8 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
